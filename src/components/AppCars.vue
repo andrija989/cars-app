7db9de7fb979e622a854b91492f614914aa2b618
Andrija Perovic <andrija989@gmail.com>
2019-07-02 18:58:02 +0200

    Zadatak 2

--- src/components/AppCars.vue
+++ src/components/AppCars.vue
@@ -1,12 +1,33 @@
 <template>
     <div>
-        
+        <ul>
+            <li v-for="(car,index) in cars" :key="index">
+                {{car}}
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
-export default {
+import { carsService } from '@/services/CarsService'
 
+export default {
+    data() {
+        return {
+            cars : []
+        }
+    },
+
+    created () {
+        carsService.getAll()
+            .then(response => {
+            this.cars = response.data
+            
+            }).catch(e => {
+            console.log(e)
+        })
+      
+    },
 }
 </script>
 
