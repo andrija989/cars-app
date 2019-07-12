78b7f1a687b6c25e0531dc329ca49d9e8a3a1a1c
Andrija Perovic <andrija989@gmail.com>
2019-07-12 20:48:04 +0200

    Zadatak 5 i 6

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,29 @@
     <nav>
       <router-link class="btn btn-primary" to="/cars">Cars</router-link>
       <router-link class="btn btn-primary" to="/add">Add Car</router-link>
+       <router-link class="nav-item nav-link" to="/login" v-if="!isAuthenticated">Login</router-link>
+      <a href="#" class="nav-item nav-link" @click="logout" v-if="isAuthenticated">Logout</a>
     </nav>
     <router-view />
   </div>
 </template>
 
 <script>
-
+import { authService } from '@/services/Auth'
 
 export default {
-  name: 'app',
-  
+  data() {
+      return {
+        isAuthenticated: authService.isAuthenticated()
+      }
+    },
+
+    methods: {
+      logout() {
+        authService.logout()
+        this.isAuthenticated = false
+      }
+    }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -6,12 +6,16 @@ Vue.use(VueRouter)
 
 import AppCars from '@/components/AppCars'
 import AddCar from '@/components/AddCar'
+import Login from '@/components/contacts/Login'
+import { authService } from './services/Auth'
+
 
 const routes = [
   { path:'/', redirect:'/cars'},
   { path:'/cars', component:AppCars},
   { path:'/edit/:id',component:AddCar},
-  { path:'/add', component:AddCar}
+  { path:'/add', component:AddCar},
+  { path: '/login', component: Login,name: 'login' }
 ]
 
 const router = new VueRouter({
@@ -25,3 +29,16 @@ new Vue({
   router: router,
   render: h => h(App),
 }).$mount('#app')
+
+router.beforeEach((to, from, next)=> {
+    
+  if(to.name !== 'login' && !authService.isAuthenticated()) {
+      return router.push( {name:'login'}); // reroute na login ako nije ulogovan
+  }
+
+  if(to.name == 'login' && authService.isAuthenticated()) {
+      return router.push({ name: from.name }) // vraca na stranicu gde si bio pre klika
+  }
+
+  next()
+})
